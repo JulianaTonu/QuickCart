f40306c8fadd0b0d64c62557b64c749501bb5b14
julianatonu <julianatonu@gmail.com>
2025-07-29 17:56:41 +0600

    inngest

--- config/inngest.js
+++ config/inngest.js
@@ -1,27 +1,42 @@
 import User from "@/models/User";
 import { Inngest } from "inngest";
+import connectDB from "./db";
 
-// Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
 
+// Create / Update User
+export const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
 
-//Inngest Function to save data to a database
-const syncUserCreation = inngest.createFunction(
-    {
-        id:'sync-user-from-clerk'
-    },
-    { event:'clerk/user.created'},
-    async({event})=>{
-        const {id, first_name, last_name, email_addresses, image_url} = event.data
-        const userData = {
-            _id:id,
-            email:email_addresses[0].email_address,
-            name:first_name + ' ' + last_name,
-            imageUrl:image_url,
-        }
-        await connectDB()
-        await User.create(userData)
-    }
-)
+    if (!id) return; // Safety check
 
-//Inngest Function to update user data in database
+    await connectDB();
+
+    await User.findByIdAndUpdate(
+      id,
+      {
+        email: email_addresses[0]?.email_address || "",
+        name: `${first_name ?? ""} ${last_name ?? ""}`.trim(),
+        imageUrl: image_url || ""
+      },
+      { upsert: true, new: true }
+    );
+  }
+);
+
+// Delete User
+export const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-with-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+
+    if (!id) return; // Safety check
+
+    await connectDB();
+    await User.findByIdAndDelete(id);
+  }
+);
